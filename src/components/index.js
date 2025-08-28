c74f4ea165bef7d4d4ecc707da2bd312808b89a4
Yağmur ŞİMŞEK <yyagmursmsk.53@gmail.com>
2025-08-28 14:28:12 +0300

    y

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-// components/index.js
-export { default as NotificationContainer } from "./DynamicExcelReader/NotificationContainer";
-export { default as UploadSection } from "./DynamicExcelReader/UploadSection";
-export { default as MappingSection } from "./DynamicExcelReader/MappingSection";
-export { default as PreviewSection } from "./DynamicExcelReader/PreviewSection";
-export { default as SummarySection } from "./DynamicExcelReader/SummarySection";
-export { default as SaveSection } from "./DynamicExcelReader/SaveSection";
-export { default as LoadingOverlay } from "./DynamicExcelReader/LoadingOverlay";
